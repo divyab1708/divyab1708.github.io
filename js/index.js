f9e132a1d86ede4ae2089a1a2b1a85a66cbca84b
divyab1708 <divyab1708@gmail.com>
2016-08-13 15:41:54 +0530

    added navbar, posters

--- js/index.js
+++ js/index.js
@@ -57,4 +57,55 @@ $(document).ready(function(){
 
     })
 
+
+    $(".posterPortfolio").on("click","a",function () {
+        var posterNum= +$(this).data("poster");
+        $("#posterGalleryModal").find(".active").removeClass("active");
+        $("#poster-carousel").find(".carousel-inner").find(".item:nth-child("+ posterNum+")").addClass("active");
+        //alert(posterNum);
+    })
+
+    $(".sideNavBar").on("click","a",function (e) {
+
+        $(".glyphicon-star").removeClass("glyphicon-star").addClass("glyphicon-star-empty");
+        $(this).removeClass("glyphicon-star-empty").addClass("glyphicon-star");
+
+        var targetDiv=$(this).attr("href");
+        $('html, body').animate({
+            scrollTop: $(targetDiv).offset().top
+        }, 2000);
+
+        e.preventDefault();
+    })
+
+    var distance1 = $('#sec1').offset().top;
+    var distance2 = $('#sec2').offset().top;
+    var distance3 = $('#sec3').offset().top;
+    var distance4 = $('#sec4').offset().top;
+        $window = $(window);
+
+    $window.scroll(function() {
+        if ( $window.scrollTop() >= distance1 && $window.scrollTop()< distance2) {
+            $(".glyphicon-star").removeClass("glyphicon-star").addClass("glyphicon-star-empty");
+            $(".sideNavBar").find("a:nth-child(1)").removeClass("glyphicon-star-empty").addClass("glyphicon-star");
+            // console.log("sec1");
+        }
+        else if ( $window.scrollTop() >= distance2 && $window.scrollTop()< distance3) {
+            $(".glyphicon-star").removeClass("glyphicon-star").addClass("glyphicon-star-empty");
+            $(".sideNavBar").find("a:nth-child(2)").removeClass("glyphicon-star-empty").addClass("glyphicon-star");
+            // console.log("sec2");
+        }
+        else if ( $window.scrollTop() >= distance3 && $window.scrollTop()< distance4) {
+            $(".glyphicon-star").removeClass("glyphicon-star").addClass("glyphicon-star-empty");
+            $(".sideNavBar").find("a:nth-child(3)").removeClass("glyphicon-star-empty").addClass("glyphicon-star");
+            // console.log("sec3");
+        }
+        else if ( $window.scrollTop() >= distance4-300 ) {
+            $(".glyphicon-star").removeClass("glyphicon-star").addClass("glyphicon-star-empty");
+            $(".sideNavBar").find("a:nth-child(4)").removeClass("glyphicon-star-empty").addClass("glyphicon-star");
+            // console.log("sec4");
+            //alert("hi");
+        }
+    });
+
 });
